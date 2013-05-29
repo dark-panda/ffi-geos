0cc15d96a046d18ea3bf0155d3fc6e4899910d28
J Smith <dark.panda@gmail.com>
2013-05-29 14:36:16 -0400

    Hard-code the expected results for Geos::LinearRing tests.

--- test/linear_ring_tests.rb
+++ test/linear_ring_tests.rb
@@ -6,11 +6,16 @@ require 'test_helper'
 class LinearRingTests < MiniTest::Unit::TestCase
   include TestHelper
 
+  def setup
+    super
+    writer.trim = true
+  end
+
   def test_to_polygon
     geom = read('POLYGON ((0 0, 5 0, 5 5, 0 5, 0 0))')
     ring = geom.exterior_ring
 
-    assert_equal(write(geom), write(ring.to_polygon))
+    assert_equal('POLYGON ((0 0, 5 0, 5 5, 0 5, 0 0))', write(ring.to_polygon))
   end
 
   def test_to_polygon_with_srid
