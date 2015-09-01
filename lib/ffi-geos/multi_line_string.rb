9cfff60016cc31ca540deb42fd92d9335bb0f4ee
J Smith <dark.panda@gmail.com>
2015-09-01 01:28:23 -0300

    Add the `Geos::MultiLineString#closed?` method.

--- lib/ffi-geos/multi_line_string.rb
+++ lib/ffi-geos/multi_line_string.rb
@@ -2,5 +2,10 @@
 
 module Geos
   class MultiLineString < GeometryCollection
+    if FFIGeos.respond_to?(:GEOSisClosed_r) && Geos::GEOS_VERSION >= '3.5.0'
+      def closed?
+        bool_result(FFIGeos.GEOSisClosed_r(Geos.current_handle, self.ptr))
+      end
+    end
   end
 end
